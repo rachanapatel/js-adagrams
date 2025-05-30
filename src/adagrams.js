a45442b7dbdf5d6be9dd0945ec555592b2ac4fce
rachanapatel <rmp226@scarletmail.rutgers.edu>
2025-05-30 00:22:58 -0400

    completed highestScoreFrom function

--- src/adagrams.js
+++ src/adagrams.js
@@ -4,7 +4,7 @@ const letterCounts = [["A", 9], ["B", 2], ["C", 2], ["D", 4], ["E", 12], ["F", 2
                       ["S", 4], ["T", 6], ["U", 4], ["V", 2], ["W", 2], ["X", 1], 
                       ["Y", 2], ["Z", 1]];
 
-const score_chart = {'A': 1,'E': 1, 'I': 1,'O': 1,'U': 1,'L': 1,'N': 1,'R': 1,'S': 1,'T': 1, 
+const scoreChart = {'A': 1,'E': 1, 'I': 1,'O': 1,'U': 1,'L': 1,'N': 1,'R': 1,'S': 1,'T': 1, 
                       'D': 2,'G': 2, 
                       'B': 3,'C': 3,'M': 3,'P': 3, 
                       'F': 4,'H': 4,'V': 4,'W': 4,'Y': 4, 
@@ -81,11 +81,50 @@ export const scoreWord = (word) => {
   }
 
   for (const char of word) {
-    score += score_chart[char.toUpperCase()];
+    score += scoreChart[char.toUpperCase()];
   }
   return score;
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  let scoreList = [];
+  for (const word of words) {
+    scoreList.push([word, scoreWord(word)])
+  }
+  let winningScore = -1;
+  let winningWord = "";
+  let ties = [];
+
+  for (const wordSet of scoreList) {
+    if (wordSet[1] > winningScore) {
+      winningScore = wordSet[1];
+      winningWord =  wordSet[0];
+      if (ties.length > 0) {
+        ties = [];
+      }
+    }
+    else if (wordSet[1] === winningScore)  {
+      ties.push([winningWord, winningScore]);
+      ties.push(wordSet);
+    }
+  }
+  if (ties.length > 0) {
+    const tieWinner = tiebreaker(ties);
+    return {word: tieWinner[0], score: tieWinner[1]};
+  }
+  return {word: winningWord, score: winningScore};
+};
+
+function tiebreaker(tiesList) {
+  let winningWordSet = tiesList[0];
+  for (const wordSet of tiesList) {
+    if (wordSet[0].length === 10) {
+      return wordSet;
+    } else {
+      if (wordSet[0].length < winningWordSet[0].length) {
+        winningWordSet = wordSet;
+      }
+    }
+  }
+  return winningWordSet;
 };
